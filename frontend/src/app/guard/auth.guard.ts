be8d15778548df344560e0f6ca172ead8d3a9d2d
Mohammed <mohammedsouleymane@outlook.com>
2022-11-08 11:14:42 +0100

    final

--- frontend/src/app/guard/auth.guard.ts
+++ frontend/src/app/guard/auth.guard.ts
@@ -6,18 +6,18 @@ import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
   providedIn: 'root'
 })
 export class AuthGuard extends KeycloakAuthGuard {
-  
+
   constructor(
     protected readonly router: Router,
     protected readonly keycloak: KeycloakService
   ) {
     super(router, keycloak);
   }
-  
+
   async isAccessAllowed(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean | UrlTree> {
-    
+
     if (!this.authenticated) {
       await this.keycloak.login({
         redirectUri: window.location.origin + state.url,
